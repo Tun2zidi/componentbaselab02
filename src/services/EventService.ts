7bb6ac376febbd499c60be6ef34dc468a63b28eb
Tun2zidi <tunsirivut@gmail.com>
2026-07-15 00:06:26 +0700

    ก่อนทำจริง

---
+++ src/services/EventService.ts
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+const apiClient = axios.create({
+    baseURL: 'https://my-json-server.typicode.com/Tun2zidi/db.josn',
+    withCredentials: false,
+    headers:{
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+    }
+})
+
+export default{
+    getEvents(){
+        return apiClient.get('/events')
+    }
+}
--- src/views/StudentView.vue
+++ src/views/StudentView.vue
@@ -1,24 +1,25 @@
 <script setup lang="ts">
 import EventCard from '@/components/EventCard.vue'
-import type {Event } from '@/types'
-import {ref , onMounted} from 'vue'
-import EventService from '@/services/EventService'
-const events = ref<Event [] | null >(null)
+import type { Event } from '@/types'
+import { ref, onMounted } from 'vue'
+import StudentService from '@/services/StudentService'
 
-onMounted (() =>{
-  EventService.getEvents()  
+const events = ref<Event[] | null>(null)
+
+onMounted(() => {
+  StudentService.getEvents()  
     .then((response) => {
       events.value = response.data
     })
-    .catch((error) =>{
-      console.error('There was an error!',error)
+    .catch((error) => {
+      console.error('There was an error!', error)
     })
 })
 </script>
 
 <template>
-  <h1>Events For Good</h1>
-  <div class = "events">
+  <h1>Student List</h1>
+  <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event"/>
   </div>
 </template>

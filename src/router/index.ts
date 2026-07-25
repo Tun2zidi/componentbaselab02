a323c32fdd472ecaa22c227d62993617261e98d0
Tun2zidi <tunsirivut@gmail.com>
2026-07-25 16:38:47 +0700

    lab03 step5.2 NetworkError

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,7 @@ import EventRegisterView from '@/views/event/RegisterView.vue'
 import EventEditView from '@/views/event/EditView.vue'
 import EventLayoutView from '@/views/event/LayoutView.vue'
 import NotFoundView from '@/views/NotFoundView.vue' 
+import NetworkErrorView from '@/views/NetworkErrorView.vue' 
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -50,6 +51,11 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: AboutView
+    },
+     {
+      path: '/network-error',
+      name: 'network-error-view',
+      component: NetworkErrorView
     },
     {
       path: '/404/:resource',

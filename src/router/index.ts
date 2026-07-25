36706f5eae649627d1dc18e5571e03df0fc83529
Tun2zidi <tunsirivut@gmail.com>
2026-07-25 16:06:59 +0700

    Lab3 step 2.9 setup size in normal 2 to other

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,14 +2,19 @@ import { createRouter, createWebHistory } from 'vue-router'
 import EventListView from '@/views/EventListView.vue'
 import AboutView from '@/views/AboutView.vue'
 import EventDetailView from '@/views/EventDetailView.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'event-list-view',
-      component: EventListView,
-    },
+   {
+  path: '/',
+  name: 'event-list-view',
+  component: EventListView,
+  props: (route) => ({
+    page: parseInt(route.query.page?.toString() || '1'),
+    perPage: parseInt(route.query.perPage?.toString() || '2')
+  })
+},
     {
       path: '/event/:id',
       name: 'event-detail-view',
--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,17 +1,36 @@
 <script setup lang="ts">
 import EventCard from '@/components/EventCard.vue'
 import type {Event } from '@/types'
-import {ref , onMounted} from 'vue'
+import {ref , onMounted , computed , watchEffect} from 'vue'
 import EventService from '@/services/EventService'
 const events = ref<Event [] | null >(null)
-
+const totalEvents = ref<number>(0)
+  const hasNextPage = computed (() =>{
+    const totalPages = Math.ceil(totalEvents.value/perPage.value)
+    return page.value < totalPages
+  })
+  const props = defineProps({
+    page:{
+      type: Number,
+      required: true
+    },
+    perPage:{
+      type:Number,
+      required:true
+    }
+  })
+  const page = computed(() => props.page)
+  const perPage = computed(() => props.perPage)
 onMounted (() =>{
-  EventService.getEvents()  
+  watchEffect(() =>{
+  EventService.getEvents(perPage.value, page.value)  
     .then((response) => {
       events.value = response.data
+      totalEvents.value = response.headers[ 'x-total-count']
     })
     .catch((error) =>{
       console.error('There was an error!',error)
+      })
     })
 })
 </script>
@@ -20,7 +39,19 @@ onMounted (() =>{
   <h1>Events For Good</h1>
   <div class = "events">
     <EventCard v-for="event in events" :key="event.id" :event="event"/>
-  </div>
+  
+  <div class = "pagination">
+  <RouterLink
+    :to="{name:'event-list-view', query:{ page:page -1, perPage:perPage}}"
+    rel="prev"
+    v-if="page !=1"
+    >&#60; Prev Page</RouterLink>
+
+    <RouterLink :to="{name: 'event-list-view', query:{ page:page + 1, perPage:perPage}}"
+    rel="next"
+    v-if="hasNextPage"
+    >Next Page &#62;</RouterLink>
+    </div></div>
 </template>
 
 <style scoped>
@@ -28,5 +59,24 @@ onMounted (() =>{
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+.pagination{
+  display:flex;
+  width: 290px;
+}
+
+.pagination a{
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50
+}
+
+#page-prev{
+  text-align: left;
+}
+
+#page-next{
+  text-align: right;
 }
 </style>

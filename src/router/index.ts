1f627e598a06de4fd4d71aee058c5fbb3c2e0754
Tun2zidi <tunsirivut@gmail.com>
2026-07-29 19:00:58 +0700

    lab4 step2.3 move pb global guard

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,74 +1,82 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventListView from '@/views/EventListView.vue'
 import AboutView from '@/views/AboutView.vue'
-import EventDetailView from '@/views/event/DetailView.vue'        
-import EventRegisterView from '@/views/event/RegisterView.vue'    
+import EventDetailView from '@/views/event/DetailView.vue'
+import EventRegisterView from '@/views/event/RegisterView.vue'
 import EventEditView from '@/views/event/EditView.vue'
 import EventLayoutView from '@/views/event/LayoutView.vue'
-import NotFoundView from '@/views/NotFoundView.vue' 
-import NetworkErrorView from '@/views/NetworkErrorView.vue' 
+import NotFoundView from '@/views/NotFoundView.vue'
+import NetworkErrorView from '@/views/NetworkErrorView.vue'
+import nProgress from 'nprogress'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-   {
-  path: '/',
-  name: 'event-list-view',
-  component: EventListView,
-  props: (route) => ({
-    page: parseInt(route.query.page?.toString() || '1'),
-    perPage: parseInt(route.query.perPage?.toString() || '2')
-  })
-},
-{
-  path: '/event/:id',
-      name: 'event-layout-view',      
-      component: EventLayoutView,
-      props: true,
-      children: [ 
-
+    {
+      path: '/',
+      name: 'event-list-view',
+      component: EventListView,
+      props: (route) => ({
+        page: parseInt(route.query.page?.toString() || '1'),
+        perPage: parseInt(route.query.perPage?.toString() || '2'),
+      }),
+    },
     {
       path: '/event/:id',
-      name: 'event-detail-view',
-      component: EventDetailView,
-      props: true
-      
-    },
-    {
-      path: '/event/:id/register',
-      name: 'event-register-view',
-      component: EventRegisterView,
-      props: true
-    },
-    {
-      path: '/event/:id/edit',
-      name: 'event-edit-view',
-      component: EventEditView,
-      props: true
-    }]
+      name: 'event-layout-view',
+      component: EventLayoutView,
+      props: true,
+      children: [
+        {
+          path: '/event/:id',
+          name: 'event-detail-view',
+          component: EventDetailView,
+          props: true,
+        },
+        {
+          path: '/event/:id/register',
+          name: 'event-register-view',
+          component: EventRegisterView,
+          props: true,
+        },
+        {
+          path: '/event/:id/edit',
+          name: 'event-edit-view',
+          component: EventEditView,
+          props: true,
+        },
+      ],
     },
     {
       path: '/about',
       name: 'about',
-      component: AboutView
+      component: AboutView,
     },
-     {
+    {
       path: '/network-error',
       name: 'network-error-view',
-      component: NetworkErrorView
+      component: NetworkErrorView,
     },
     {
       path: '/404/:resource',
       name: '404-resource-view',
       component: NotFoundView,
-      props: true
+      props: true,
     },
     {
-      path: '/:catchAll(.*)',     
+      path: '/:catchAll(.*)',
       name: 'not-found',
-      component: NotFoundView
-    }
+      component: NotFoundView,
+    },
   ],
+})
+
+router.beforeEach(() => {
+  nProgress.start()
+})
+
+router.afterEach(() => {
+  nProgress.done()
 })
 
 export default router
--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,57 +1,68 @@
 <script setup lang="ts">
 import EventCard from '@/components/EventCard.vue'
-import type {Event } from '@/types'
-import {ref , onMounted , computed , watchEffect} from 'vue'
+import type { Event } from '@/types'
+import { ref, onMounted, computed, watchEffect } from 'vue'
 import EventService from '@/services/EventService'
-const events = ref<Event [] | null >(null)
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const events = ref<Event[] | null>(null)
 const totalEvents = ref<number>(0)
-  const hasNextPage = computed (() =>{
-    const totalPages = Math.ceil(totalEvents.value/perPage.value)
-    return page.value < totalPages
-  })
-  const props = defineProps({
-    page:{
-      type: Number,
-      required: true
-    },
-    perPage:{
-      type:Number,
-      required:true
-    }
-  })
-  const page = computed(() => props.page)
-  const perPage = computed(() => props.perPage)
-onMounted (() =>{
-  watchEffect(() =>{
-  EventService.getEvents(perPage.value, page.value)  
-    .then((response) => {
-      events.value = response.data
-      totalEvents.value = response.headers[ 'x-total-count']
-    })
-    .catch((error) =>{
-      console.error('There was an error!',error)
+
+const props = defineProps({
+  page: {
+    type: Number,
+    required: true,
+  },
+  perPage: {
+    type: Number,
+    required: true,
+  },
+})
+const page = computed(() => props.page)
+const perPage = computed(() => props.perPage)
+
+const hasNextPage = computed(() => {
+  const totalPages = Math.ceil(totalEvents.value / perPage.value)
+  return page.value < totalPages
+})
+
+onMounted(() => {
+  events.value = null
+  watchEffect(() => {
+    EventService.getEvents(perPage.value, page.value)
+      .then((response) => {
+        events.value = response.data
+        totalEvents.value = response.headers['x-total-count']
       })
-    })
+      .catch(() => {
+        router.push({ name: 'network-error-view' })
+      })
+  })
 })
 </script>
 
 <template>
   <h1>Events For Good</h1>
-  <div class = "events">
-    <EventCard v-for="event in events" :key="event.id" :event="event"/>
-  
-  <div class = "pagination">
-  <RouterLink
-    :to="{name:'event-list-view', query:{ page:page -1, perPage:perPage}}"
-    rel="prev"
-    v-if="page !=1"
-    >&#60; Prev Page</RouterLink>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
 
-    <RouterLink :to="{name: 'event-list-view', query:{ page:page + 1, perPage:perPage}}"
-    rel="next"
-    v-if="hasNextPage"
-    >Next Page &#62;</RouterLink>
-    </div></div>
+    <div class="pagination">
+      <RouterLink
+        :to="{ name: 'event-list-view', query: { page: page - 1, perPage: perPage } }"
+        rel="prev"
+        v-if="page != 1"
+        >&#60; Prev Page</RouterLink
+      >
+
+      <RouterLink
+        :to="{ name: 'event-list-view', query: { page: page + 1, perPage: perPage } }"
+        rel="next"
+        v-if="hasNextPage"
+        >Next Page &#62;</RouterLink
+      >
+    </div>
+  </div>
 </template>
 
 <style scoped>
@@ -61,22 +72,22 @@ onMounted (() =>{
   align-items: center;
 }
 
-.pagination{
-  display:flex;
+.pagination {
+  display: flex;
   width: 290px;
 }
 
-.pagination a{
+.pagination a {
   flex: 1;
   text-decoration: none;
-  color: #2c3e50
+  color: #2c3e50;
 }
 
-#page-prev{
+#page-prev {
   text-align: left;
 }
 
-#page-next{
+#page-next {
   text-align: right;
 }
 </style>

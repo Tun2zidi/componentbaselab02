bbff79847dcc2a9917b730d7716f5576ce803487
Tun2zidi <tunsirivut@gmail.com>
2026-08-09 10:08:23 +0700

    l5 /4.1 explain

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -8,36 +8,27 @@ defineProps<{
 </script>
 
 <template>
-  <RouterLink class="event-link" :to ="{name: 'event-detail-view',params:{
-    id: event.id
-  }}">
-  <div class="event-class">
-    <h2>{{ event.title }}</h2>
-    <span>@{{ event.time }} on {{ event.date }}</span>
-    <EventMeta :event="event" />
+  <RouterLink
+    class="event-link"
+    :to="{
+      name: 'event-detail-view',
+      params: {
+        id: event.id,
+      },
+    }"
+  >
+    <div
+      class="p-5 w-[250px] cursor-pointer border border-[#39495c] mb-[18px] hover:scale-101 hover:shadow-sp"
+    >
+      <h2>{{ event.title }}</h2>
+      <span>@{{ event.time }} on {{ event.date }}</span>
+      <EventMeta :event="event" />
     </div>
   </RouterLink>
-  
 </template>
 
 <style scoped>
-.event-class {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  padding: 20px;
-  width: 250px;
-  cursor: pointer;
-  border: 1px solid #39395c;
-  margin-bottom: 18px;
-}
-
-.event-class:hover {
-  transform: scale(1.01);
-  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
-}
-
-.event-link{
+.event-link {
   text-decoration: none;
   color: #2c3e50;
 }
